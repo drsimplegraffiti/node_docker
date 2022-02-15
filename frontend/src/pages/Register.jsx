11fb94ccf637b921bbb4d72439a2eba8d553409e
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-02-15 02:30:13 +0100

    login and logout func added

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from 'react';
-import { FaUser } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify'
+import { FaUser } from 'react-icons/fa';
+import { register, reset } from '../features/auth/authSlice'
+
+import Spinner from '../components/Spinner';
+
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -10,6 +17,28 @@ function Register() {
     })
 
     const { name, email, password, password2 } = formData
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {
+        user,
+        isLoading,
+        isError,
+        isSuccess,
+        message
+    } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+
+        if (isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset)
+    }, [user, isError, isSuccess, message, navigate, dispatch])
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -18,6 +47,20 @@ function Register() {
     }
     const onSubmit = (e) => {
         e.preventDefault()
+        if (password !== password2) {
+            toast.error('Password does not match')
+        } else {
+            const userData = {
+                name,
+                email,
+                password
+            }
+            dispatch(register(userData))
+        }
+    }
+
+    if (isLoading) {
+        return <Spinner />
     }
     return (
         <>
